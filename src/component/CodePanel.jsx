984d53c080f155e8fafa5ed8a80c4893fde562bd
Nissan-19 <nissanmalta22@gmail.com>
2026-08-16 20:08:13 +0200

    feat: add syntax highlighting and line numbers to code panel

--- src/component/CodePanel.jsx
+++ src/component/CodePanel.jsx
@@ -1,4 +1,10 @@
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism"
+
 function CodePanel({ codeLines, activeLineNumber }) {
+
+  const codeString = codeLines.join("\n")
+
   return (
     <section className="mt-2 flex min-h-125 w-full flex-col overflow-hidden rounded-xl border border-slate-700 bg-slate-900 shadow-lg shadow-black/20">
       <div className="flex border-b border-slate-700 bg-slate-800/80 px-3 py-2">
@@ -12,31 +18,41 @@ function CodePanel({ codeLines, activeLineNumber }) {
       </div>
 
       <div className="flex-1 overflow-x-auto bg-[#0d1117]">
-        <pre className="min-h-full p-3 text-sm leading-6">
-          <code className="block min-w-full">
-            {codeLines.map((codeLine, index) => (
-              <span
-                key={index}
-                className={
-                  index + 1 === activeLineNumber
-                    ? "active-code-line block rounded-sm px-2"
-                    : "block px-2 text-slate-300"
-                }
-              >
-                {codeLine || "\u00A0"}
-              </span>
-            ))}
-          </code>
-        </pre>
+        <SyntaxHighlighter
+    
+          language="javascript"
+          style={vscDarkPlus}
+          showLineNumbers
+          wrapLines //SyntaxHighlighter treats all your code as one large bloc, warplines wraps every code line in its own HTML element:
+          lineProps={(lineNumber) => { //lineProps is a function that SyntaxHighlighter automatically runs for every wrapped line. It gives us that line’s number:
+            if(lineNumber === activeLineNumber){
+              return{
+                className:"active-code-line block pl-3"
+              }
+            } 
+            return{
+              className:"block pl-3"
+            }
+          }}
+          customStyle={{
+            margin: 0,
+            minHeight:"100%",
+            background:"#0d1117",
+            padding:"0.75rem",
+            lineHeight: "1.75rem",
+            
+          }}
+          codeTagProps={{
+            style: {
+              fontSize: "1.125rem",              
+            },
+          }}>
+            {codeString}
+        </SyntaxHighlighter>
       </div>
     </section>
   )
 }
 
-/*
-  <pre> preserves spaces and line breaks.
-  <code> identifies the content as programming code.
-  "\u00A0" preserves intentional empty lines.
-*/
 
 export default CodePanel
